ac0c6bdf46dc85be883655e8527229b880d62448
Rohan Basankar <r.basankarr@gmail.com>
2025-07-06 12:45:44 +0530

    fix(index): correct syntax error in User model definition

--- level1/user-service/src/index.ts
+++ level1/user-service/src/index.ts
@@ -17,7 +17,7 @@ const userSchema = new mongoose.Schema({
   name: String
 });
 
-const User = mongoose.model('User', userSchema;
+const User = mongoose.model('User', userSchema);
 // test changes
 // REST API
 app.get('/api/users', async (req, res) => {
